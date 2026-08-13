ef6aebfe8e2f7f0147e15d407e416f7f1d274631
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-13 22:26:03 +0530

    Add files via upload

---
+++ practise programs/Encapsulation.java
@@ -0,0 +1,22 @@
+
+    
+class Student {
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setAge(20);
+        System.out.println(s.getAge());
+    }
+}
+        
