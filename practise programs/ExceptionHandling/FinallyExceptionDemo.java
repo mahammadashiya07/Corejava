74e12796d67833896948f70592cbb5cc3e251048
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-28 12:06:46 +0530

    Add files via upload

---
+++ practise programs/ExceptionHandling/FinallyExceptionDemo.java
@@ -0,0 +1,17 @@
+public class FinallyExceptionDemo {
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 0;
+            System.out.println(result);
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Exception: Cannot divide by zero");
+        }
+
+        finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
