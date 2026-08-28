d5fa3cba355eef5dd6a5abb1170ed663ea7313ed
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-28 10:34:36 +0530

    Add files via upload

---
+++ practise programs/DivisionExample.java
@@ -0,0 +1,13 @@
+class DivisionExample {
+    public static void main(String[] args) {
+        try {
+            int x = 10;
+            int y = 0;
+            int result = x/y;
+            System.out.println("Result = " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("cannot divisible by zero");
+        }
+    }
+}
