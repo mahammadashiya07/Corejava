b0f66927851abaae35925ca351b8cdbadf165dd6
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-29 19:45:08 +0530

    Update c1 implements I1.java

--- Assignment /Assignment 2/c1 implements I1.java
+++ Assignment /Assignment 2/c1 implements I1.java
@@ -1,1 +1,26 @@
+interface I1 {
 
+    void displayI1();
+}
+
+class C1 implements I1 {
+
+    public void displayI1() {
+        System.out.println("This method is implemented from I1");
+    }
+
+    void displayC1() {
+        System.out.println("This is Class C1");
+    }
+}
+
+public class Question3 {
+
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.displayI1();
+        obj.displayC1();
+    }
+}
