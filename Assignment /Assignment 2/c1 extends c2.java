e525a4def3f085dc10f792bc55c5fd535bfea282
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-29 19:39:06 +0530

    Update c1 extends c2.java

--- Assignment /Assignment 2/c1 extends c2.java
+++ Assignment /Assignment 2/c1 extends c2.java
@@ -1,1 +1,24 @@
+class C2 {
 
+    void displayC2() {
+        System.out.println("This is Class C2");
+    }
+}
+
+class C1 extends C2 {
+
+    void displayC1() {
+        System.out.println("This is Class C1");
+    }
+}
+
+public class Question1 {
+
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.displayC2();
+        obj.displayC1();
+    }
+}
