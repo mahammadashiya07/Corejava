a80ac96744402254898e005967d04747743867da
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-08-29 19:51:33 +0530

    Update c1 implements I1, I2.java

--- Assignment /Assignment 2/c1 implements I1, I2.java
+++ Assignment /Assignment 2/c1 implements I1, I2.java
@@ -1,1 +1,36 @@
+interface I1 {
 
+    void displayI1();
+}
+
+interface I2 {
+
+    void displayI2();
+}
+
+class C1 implements I1, I2 {
+
+    public void displayI1() {
+        System.out.println("Method of Interface I1");
+    }
+
+    public void displayI2() {
+        System.out.println("Method of Interface I2");
+    }
+
+    void displayC1() {
+        System.out.println("Method of Class C1");
+    }
+}
+
+public class Question {
+
+    public static void main(String[] args) {
+
+        C1 obj = new C1();
+
+        obj.displayI1();
+        obj.displayI2();
+        obj.displayC1();
+    }
+}
