ded4ae5f537f711c1db626125115d15ad0478111
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-06 10:41:04 +0530

    Add files via upload

---
+++ observation/Exceptions /ArrayIndexOutOfBounds/ArrayIndexDemo.java
@@ -0,0 +1,10 @@
+class ArrayIndexDemo {
+    public static void main(String[] args) {
+        try {
+            int[] numbers = {10, 20, 30};
+            System.out.println(numbers[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception: Array index is out of bounds");
+        }
+    }
+}
