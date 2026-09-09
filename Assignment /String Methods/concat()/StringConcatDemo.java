4e0346ad1656aafb85b8063526903ab37191abdb
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:12:29 +0530

    Add files via upload

---
+++ Assignment /String Methods/concat()/StringConcatDemo.java
@@ -0,0 +1,9 @@
+class StringConcatDemo {
+    public static void main(String[] args) {
+        String greetingText = "Beautiful ";
+        String personName = "Weather";
+
+        System.out.println("Combined: "
+                + greetingText.concat(personName));
+    }
+}
