c1cfd520d9ccec2da0c259d314f966fc09c4e99e
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:08:05 +0530

    Add files via upload

---
+++ Assignment /String Methods/trim()/StringTrimDemo.java
@@ -0,0 +1,8 @@
+class StringTrimDemo {
+    public static void main(String[] args) {
+        String addressText = "   Hyderabad   ";
+
+        System.out.println("Before: [" + addressText + "]");
+        System.out.println("After: [" + addressText.trim() + "]");
+    }
+}
