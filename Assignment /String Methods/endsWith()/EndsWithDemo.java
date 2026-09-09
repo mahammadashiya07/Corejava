d6f6354f8089732500fe79f3f92be82d0ba0eb98
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:11:01 +0530

    Add files via upload

---
+++ Assignment /String Methods/endsWith()/EndsWithDemo.java
@@ -0,0 +1,8 @@
+class EndsWithDemo {
+    public static void main(String[] args) {
+        String fileName = "Student.java";
+
+        System.out.println("Ends with .java: "
+                + fileName.endsWith(".java"));
+    }
+}
