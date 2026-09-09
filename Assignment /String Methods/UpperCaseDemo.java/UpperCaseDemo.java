7dc99d6ffcb683b736e7fb9a34756e5b5c445ccc
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:41:23 +0530

    Add files via upload

---
+++ Assignment /String Methods/UpperCaseDemo.java/UpperCaseDemo.java
@@ -0,0 +1,8 @@
+class UpperCaseDemo {
+    public static void main(String[] args) {
+        String cityName = "hyderabad";
+
+        System.out.println("Original: " + cityName);
+        System.out.println("Uppercase: " + cityName.toUpperCase());
+    }
+}
