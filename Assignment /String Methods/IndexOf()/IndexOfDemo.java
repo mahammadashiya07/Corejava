feaf18eb004026b11f3fa467ef06eadf1963b055
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 20:37:01 +0530

    Add files via upload

---
+++ Assignment /String Methods/IndexOf()/IndexOfDemo.java
@@ -0,0 +1,8 @@
+class IndexOfDemo {
+    public static void main(String[] args) {
+        String subjectName = "Programming";
+
+        System.out.println("Index of 'a': "
+                + subjectName.indexOf('a'));
+    }
+}
