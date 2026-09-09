74636b08269c54ec0ef45a953790e97c18976856
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:50:09 +0530

    Delete Assignment /String Methods/UpperCaseDemo.java directory

--- Assignment /String Methods/UpperCaseDemo.java/UpperCaseDemo.java
+++
@@ -1,8 +0,0 @@
-class UpperCaseDemo {
-    public static void main(String[] args) {
-        String cityName = "hyderabad";
-
-        System.out.println("Original: " + cityName);
-        System.out.println("Uppercase: " + cityName.toUpperCase());
-    }
-}
