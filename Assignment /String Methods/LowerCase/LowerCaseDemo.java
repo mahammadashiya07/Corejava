756abc1d27409e28bc98ad0e9f720fd8b03416e0
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:45:43 +0530

    Add files via upload

---
+++ Assignment /String Methods/LowerCase/LowerCaseDemo.java
@@ -0,0 +1,8 @@
+class LowerCaseDemo {
+    public static void main(String[] args) {
+        String collegeName = "ENGINEERING COLLEGE";
+
+        System.out.println("Original: " + collegeName);
+        System.out.println("Lowercase: " + collegeName.toLowerCase());
+    }
+}
