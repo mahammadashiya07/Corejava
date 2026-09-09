85548198c4bd35bc5bf98ddee849666ebda0e52e
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:05:10 +0530

    Add files via upload

---
+++ Assignment /String Methods/lastIndexOf()/LastIndexDemo.java
@@ -0,0 +1,8 @@
+class LastIndexDemo {
+    public static void main(String[] args) {
+        String languageName = "Programming";
+
+        System.out.println("Last index of 'g': "
+                + languageName.lastIndexOf('g'));
+    }
+}
