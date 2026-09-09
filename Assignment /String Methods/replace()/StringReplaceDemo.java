26eb0beb04126d26208c222c8fffc93d305978a3
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:06:39 +0530

    Add files via upload

---
+++ Assignment /String Methods/replace()/StringReplaceDemo.java
@@ -0,0 +1,9 @@
+class StringReplaceDemo {
+    public static void main(String[] args) {
+        String technologyName = "I like python";
+
+        System.out.println("Original: " + technologyName);
+        System.out.println("Updated: "
+                + technologyName.replace("python", "Java"));
+    }
+}
