7d7dd8bdd68184f476c1e3cf44d201fd30fecd02
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:25:49 +0530

    Add files via upload

---
+++ Assignment /String Methods/length()/StringLengthDemo.java
@@ -0,0 +1,8 @@
+class StringLengthDemo {
+    public static void main(String[] args) {
+        String studentName = "vamsi krishna";
+
+        System.out.println("Name: " + studentName);
+        System.out.println("Length: " + studentName.length());
+    }
+}
