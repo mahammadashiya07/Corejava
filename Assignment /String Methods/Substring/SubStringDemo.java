c3b5e682d8cc819db8c92fa4c971b93c3565e3da
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 19:19:23 +0530

    Add files via upload

---
+++ Assignment /String Methods/Substring/SubStringDemo.java
@@ -0,0 +1,9 @@
+class SubstringDemo {
+    public static void main(String[] args) {
+        String programTitle = "Programming";
+
+        System.out.println("Original: " + programTitle);
+        System.out.println("Substring: "
+                + programTitle.substring(0, 7));
+    }
+}
