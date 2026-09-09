c683f0376891c4f860127bb9cc4010b9a3ebed2e
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:54:11 +0530

    Add files via upload

---
+++ Assignment /String Methods/equals()/StringEqualsDemo.java
@@ -0,0 +1,9 @@
+class StringEqualsDemo {
+    public static void main(String[] args) {
+        String firstWord = "Java";
+        String secondWord = "Java";
+
+        System.out.println("Are strings equal? "
+                + firstWord.equals(secondWord));
+    }
+}
