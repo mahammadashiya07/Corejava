50b1a3415ffa0b57e96a4000c55bf149e886dd11
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 19:17:19 +0530

    Add files via upload

---
+++ Assignment /String Methods/Contains()/StringContainsDemo.java
@@ -0,0 +1,8 @@
+class StringContainsDemo {
+    public static void main(String[] args) {
+        String messageText = "Java is very easy";
+
+        System.out.println("Contains 'easy': "
+                + messageText.contains("easy"));
+    }
+}
