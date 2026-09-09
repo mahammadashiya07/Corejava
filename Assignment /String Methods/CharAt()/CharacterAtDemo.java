ea0a4f947d6687ee6187cf2884b99679eea68a12
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 18:33:32 +0530

    Add files via upload

---
+++ Assignment /String Methods/CharAt()/CharacterAtDemo.java
@@ -0,0 +1,8 @@
+class CharacterAtDemo {
+    public static void main(String[] args) {
+        String courseName = "java programming";
+
+        System.out.println("Course: " + courseName);
+        System.out.println("Character at index 1: " + courseName.charAt(1));
+    }
+}
