ea38f2306413c0c97184ad708c40643dbb7c8fbd
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-09 21:09:32 +0530

    Add files via upload

---
+++ Assignment /String Methods/startWith()/StartsWithDemo.java
@@ -0,0 +1,8 @@
+class StartsWithDemo {
+    public static void main(String[] args) {
+        String websiteName = "JavaProgramming";
+
+        System.out.println("Starts with Java: "
+                + websiteName.startsWith("Java"));
+    }
+}
