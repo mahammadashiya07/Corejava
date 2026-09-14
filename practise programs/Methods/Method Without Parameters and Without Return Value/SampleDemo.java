6d39e10669019cc13bff446d40b6d02f5eed34dd
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-14 10:00:19 +0530

    Add files via upload

---
+++ practise programs/Methods/Method Without Parameters and Without Return Value/SampleDemo.java
@@ -0,0 +1,11 @@
+class SampleDemo {
+
+    void sample() {
+        System.out.println("Hello, Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+        SampleDemo obj = new SampleDemo();
+        obj.sample();
+    }
+}
