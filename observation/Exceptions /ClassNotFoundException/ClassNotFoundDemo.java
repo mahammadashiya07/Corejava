6ae70c1b50107fc1c060bd3ad1e048f2f4588d96
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-16 15:25:26 +0530

    Add files via upload

--- observation/Exceptions /ClassNotFoundException/ClassNotFoundDemo.java
+++ observation/Exceptions /ClassNotFoundException/ClassNotFoundDemo.java
@@ -1,8 +1,21 @@
+// Class to demonstrate ClassNotFoundException
 class ClassNotFoundDemo {
+
+    // Main method - program execution starts here
     public static void main(String[] args) {
+
+        // try block contains code that may cause an exception
         try {
+
+            // Try to load a class using its class name
+            // "UnknownClass" does not exist, so an exception occurs
             Class.forName("UnknownClass");
-        } catch (ClassNotFoundException e) {
+        }
+
+        // Catch block handles ClassNotFoundException
+        catch (ClassNotFoundException e) {
+
+            // Display error message
             System.out.println("Exception: Class not found");
         }
     }
