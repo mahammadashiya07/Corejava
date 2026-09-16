b220db525a1cb97937e5dcfc8173faaef10a4248
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-16 15:23:43 +0530

    Add files via upload

--- observation/Exceptions /ClassCastException/ClassCastDemo.java
+++ observation/Exceptions /ClassCastException/ClassCastDemo.java
@@ -1,10 +1,27 @@
+// Class to demonstrate ClassCastException
 class ClassCastDemo {
+
+    // Main method - program execution starts here
     public static void main(String[] args) {
+
+        // try block contains code that may cause an exception
         try {
+
+            // Store an Integer object inside an Object reference
             Object value = Integer.valueOf(100);
+
+            // Try to convert Integer object into String
+            // This causes ClassCastException
             String text = (String) value;
+
+            // Print the converted value
             System.out.println(text);
-        } catch (ClassCastException e) {
+        }
+
+        // Catch block handles ClassCastException
+        catch (ClassCastException e) {
+
+            // Display error message
             System.out.println("Exception: Invalid type casting");
         }
     }
