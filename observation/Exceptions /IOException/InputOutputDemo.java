4e93437687da14c6a26b2bea4b856eeb134b0aab
mahammadashiya07 <mahammadashiya07@gmail.com>
2026-09-16 19:01:42 +0530

    Add files via upload

--- observation/Exceptions /IOException/InputOutputDemo.java
+++ observation/Exceptions /IOException/InputOutputDemo.java
@@ -1,10 +1,14 @@
-import java.io.IOException;
+import java.io.IOException;  // Import IOException class
 
 class InputOutputDemo {
     public static void main(String[] args) {
+        
         try {
+            // Manually throw an IOException
             throw new IOException("Input or output error");
+            
         } catch (IOException e) {
+            // Catch the IOException and display an error message
             System.out.println("Exception: Input/Output error occurred");
         }
     }
